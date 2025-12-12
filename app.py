2c1326baf23094ac84085323578780dbd40ba2ac
opy <140424822+optestcertified@users.noreply.github.com>
2025-12-12 14:22:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=3000, debug=True)
 
